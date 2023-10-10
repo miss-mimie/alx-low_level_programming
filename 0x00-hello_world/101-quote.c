673f6e1918e884776684862ceafd9c6731d35d76
miss-mimie <mariam.musah55@gmail.com>
2023-10-09 18:36:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
  * main - this prints exactly the input on it
  * Return: always 1.
  */
-int main(void0=)
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2915-10-19\n" 59);
 	return (1);
