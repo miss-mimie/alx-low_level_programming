ad052c773138217380abc8907e5e6d1e3820d5a0
miss-mimie <mariam.musah55@gmail.com>
2023-10-09 18:33:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - this prints exactly the input on it
  * Return: always 1.
  */
 int main(void0=)
 {
-	writ(2, "and that piece of art is useful\" -Dora Korpar, 2915-10-19\n" 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2915-10-19\n" 59);
 	return (1);
 }
