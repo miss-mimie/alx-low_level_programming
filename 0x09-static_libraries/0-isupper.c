b20b2c771db1961b17ca0840e350c17ee73db76c
miss-mimie <mariam.musah55@gmail.com>
2023-10-31 11:35:49 -0700

    0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - check for uppercase letters
+ * @c:  char to check 
+ *
+ * Return: if uppercase return 1, else return o
+ */
+int _isupper(int c)
+{
+	if ( c >= 'A' && c <= 'z' )
+		return (1);
+}
