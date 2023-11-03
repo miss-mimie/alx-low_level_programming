fe65c8bb77cbf39cf7a6918c2f44c9a488d28e26
miss-mimie <mariam.musah55@gmail.com>
2023-11-02 18:30:53 -0700

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,7 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
-
-#define NULL ((void *)0)
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,5 +23,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
