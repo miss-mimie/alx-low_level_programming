a66c12728e59bcf3cf60d8968a2dd811a9c00c5f
miss-mimie <mariam.musah55@gmail.com>
2023-11-07 12:09:33 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,84 +1,51 @@
 #include "dog.h"
 #include <stdlib.h>
 
-char *_strdup(const char *str);
-unsigned int _strlen(const char *s);
-
 /**
- * new_dog - this creates a new dog
- * @name: this is the name of the dog
- * @age: this is the age of the dog
- * @owner: this is the name of the dog's owner
+ * new_dog - function that creates a new dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
  *
- * Return: A pointer to the newly created dog
+ * Reurn: struct dog, on fail reurns NULL.
  */
-dog_t new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	/* allocate memory for the new instance of the dog entity */
-	dog_t *dog = malloc(sizeof(dog_t));
+	dog_t *new_dog;
+	int i, lname, lowner;
 
-	/* add dog details and also handle memory allocation failures */
-	dog->name = _strdup(name);
-	if (dog->name == NULL)
+	new_dog = malloc(sizeof(*new_dog));
+	if (new_dog == NULL || !(name) || !(owner))
 	{
-		free(dog);
-		return (NULL); /* memory allocation failed - clean up and leave */
-	}
-
-	dog->owner = _strdup(ownwe);
-	if (dog->owner == NULL)
-	{
-		free(dog->name); /* memory allocation failed while duplicating owner name */
-	}
-
-	dog->age = age; /* save the age of the dog */
-
-	return (dog);
-}
-
-/**
- * _strdup - this duplicates a string
- * @str: this is a string to duplicate
- *
- * Description: The _strdup() function returns a pointer to a newly allocated
- * space in memory, which contains a copy of the string @str.
- * Memory for the new string is obtained with
- * malloc() and can be freed with free()
- *
- * Return: A pointer to the duplicated string, NULL if it fails
- */
-char *_strdup(const char *str)
-{
-	char *dup_str;
-	int i = 0;
-
-	if (str == NULL)
-		return (NULL); /* handle invalid string */
-
-	dup_str = (char *) malloc(sizeof(char) * (_strlen(str) + 1));
-
-	if (dup_str == NULL)
+		free(new_dog);
 		return (NULL);
-
-	/* duplicate string - a copy process */
-	while (str[i] != '\0')
-	{
-		dup_str[i] = '\0';
-
-		return (dup_str);
 	}
 
-	/**
-	 * _strlen - Get the length of a string
-	 * @s: string
-	 *
-	 * Return: The length of string @s
-	 */
-	unsigned int _strlen(const char *s)
-	{
-		unsigned int len;
+	for (lname = 0; name[lname]; lname++)
+		;
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
 
-		for (len = 0; s[len] != '\0'; len++)
-			;
-		return (len);
+	new_dog->name = malloc(lname + 1);
+	new_dog->owner = malloc(lowner + 1);
+
+	if (!(new_dog->name) || !(new_dog->owner))
+	{
+		free(new_dog->owner);
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
 	}
+
+	for (i = 0; i < lname; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
+
+	new_dog->age = age;
+
+	for (i = 0; i < lowner; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
+
+	return (new_dog);
+}
