30679baf59de1c58b88a5cc90a210ac8d221db86
miss-mimie <mariam.musah55@gmail.com>
2023-11-08 02:57:14 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "_function_pointers.h_"
+#include <stdio.h>
+
+/**
+ * print_name - this prints the name
+ * @name: this is the name to print
+ * @fptr: this is a function pointer
+ */
+void print_name(char *name, void (*fptr)(char *))
+{
+	if (!name || *name == '\0' || !fptr)
+		/* either the name or the function pointer is NULL */
+		return;
+
+	/* call the function pointer for further processing */
+	fptr(name);
+}
