f4e29bc7367a85b37b07a086da57c199e27ee7f3
miss-mimie <mariam.musah55@gmail.com>
2024-06-20 20:12:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - This will display my first hello world
+ * main - prints my first hello world in c
  * return 0.
  */
-
-int main(void)
+int main(void);
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
