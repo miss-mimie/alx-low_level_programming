74a800a431942cc826e172170021b8d52516fce1
miss-mimie <mariam.musah55@gmail.com>
2024-06-20 20:23:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the outcome of proper grammar in coding
+ * return (0)
+ */
+
+int main(void);
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
