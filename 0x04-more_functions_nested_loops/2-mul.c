c4dd4726fbdd7d4d7a0d9600b080cb7d29ba9554
miss-mimie <mariam.musah55@gmail.com>
2024-06-29 09:04:03 -0700

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * Description: checks the multiples of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Product of a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
