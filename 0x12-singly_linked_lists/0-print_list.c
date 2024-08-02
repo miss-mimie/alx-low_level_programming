6a16ff3efffdaec82e22d4c207333538afff7ee0
miss-mimie <mariam.musah55@gmail.com>
2024-08-01 18:25:03 -0700

    Node task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_list - This prints all elements of list_t.
- * @head: this is the head node
+ * print_list - This prints all element of a list_t.
+ * @head: the head node for this function.
  *
- * Return: the number of nodes
+ * Return: the number of nodes.
  */
-size_t print_list(const lsit_t *head)
+size_t print_list(const list_t *head)
 {
 	size_t node_count = 0;
 
@@ -22,7 +22,7 @@ size_t print_list(const lsit_t *head)
 		else
 			printf("[%u] %s\n", head->len, head->str);
 		node_count++;
-		head = head->nest;
+		head = head->next;
 	}
 
 	return (node_count);
