0d3d68ce527e1b72e068808786932b8290882322
miss-mimie <mariam.musah55@gmail.com>
2024-08-08 21:51:27 -0700

    Task 4

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+/**
+ * get_endianness - this checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int n;
+	char *c;
+
+	n = 10;
+	c = (char *)&n;
+
+	return (*c);
+}
