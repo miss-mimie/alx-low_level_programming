63257287e6769a33c2aac6e9af6192be4dc2c758
miss-mimie <mariam.musah55@gmail.com>
2024-08-08 21:32:41 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,8 +4,8 @@
 #define _atoi(c) ((c) - '0')
 
 /**
- * binary_to_uint - converts a binary to an unsigned int.
- * @b: points to a string of 0 and 1 chars.
+ * binary_to_uint - this converts a binary to an unsigned integer
+ * @b: the str containing the binary value to be converted
  *
  * Return: base 10 representation of binary digit @b,
  * else 0 if b is either NULL or a non-binary digit
@@ -23,9 +23,10 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (is_non_binary(*b))
 		{
-			return (0); 
+			return (0);
 		}
 
+		/* this performs base 2 to base 10 arithmetic */
 		base_10 = (base_10 << 1) | (_atoi(*b));
 		b++;
 	}
