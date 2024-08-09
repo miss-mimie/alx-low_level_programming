a9146dfd23614c779e7dd9b51791bdbaf24d4861
miss-mimie <mariam.musah55@gmail.com>
2024-08-08 21:46:10 -0700

    Task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#define BOUNDARY 63 /* 0 through 64, exclusive */
+
+/**
+ * clear_bit - this sets the value of a bit to zero at a given index
+ * @n: number to clear bit for
+ * @index: index at which the bit is to be cleared
+ *
+ * Return: 1 if bit was cleared successful, else -1 for error.
+ * The error condition in this case is when the index is greater than
+ * boundary accepted for bit on a 64-bit machine
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > BOUNDARY)
+	{
+		return (-1);
+	}
+
+	/*this set the mask and clear the ith bit with it */
+	*n &= ~(1 << index);
+
+	return (1);
+}
