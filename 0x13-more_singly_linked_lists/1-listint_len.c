73c948f30ef3a142bab27e31dacd6fc8e60616fc
miss-mimie <mariam.musah55@gmail.com>
2024-08-08 22:05:26 -0700

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @head: the head node
+ *
+ * Return: the number of elements in a linked list of integers
+ */
+
+size_t listint_len(const listint_t *head)
+{
+	size_t node_count = 0;
+
+	if (head == NULL)
+		return (0); /* returns an empty list */
+
+	/* the list is not empty, get the number of elements */
+	while (head != NULL)
+	{
+		node_count++;
+		head = head->next;
+	}
+
+	return (node_count);
+}
